494cce2be13f0634c61cf1a2054fda9c2c256b6d
Danielson <caosdan@gmail.com>
2023-01-08 07:46:21 -0300

    Add files via upload

--- controladormidi/main.js
+++ controladormidi/main.js
@@ -1,6 +1,6 @@
   // Enable WEBMIDI.js and trigger the onEnabled() function when ready
   WebMidi
-    .enable()
+    .enable({sysex:true})
     .then(onEnabled)
     .catch(err => alert(err));
 
@@ -15,12 +15,24 @@
       document.body.innerHTML+= "No device detected.";
     } else {
       WebMidi.inputs.forEach((device, index) => {
-        document.body.innerHTML+= `${index}: ${device.name} <br>`;
+        //document.body.innerHTML+= `${index}: ${device.name} <br>`;
+        document.getElementById('nomeDispositivos').innerHTML += `${index}: ${device.name} <br>`;
       });
 
-      WebMidi.inputs[0].addForwarder(WebMidi.outputs[1]); 
+      // WebMidi.inputs[0].addForwarder(WebMidi.outputs[1]); 
 
+      WebMidi.outputs[0].sendControlChange(0,87,1);
+      WebMidi.outputs[0].sendControlChange(32, 64,1);
+      WebMidi.outputs[0].sendProgramChange(10,1);
+      
     }
 
   }
   
+
+  function PlayNota(){
+    let output = WebMidi.outputs[0];
+    let channel = output.channels[1];
+    channel.playNote("C3", {duration: 1000});
+      
+}
