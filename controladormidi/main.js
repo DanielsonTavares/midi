30250aefeb4671ddf2650839fd1d1285d7f4955b
Danielson <caosdan@gmail.com>
2023-01-08 09:50:31 -0300

    Adicionando troca de instrumentos

--- controladormidi/main.js
+++ controladormidi/main.js
@@ -1,3 +1,5 @@
+  
+
   // Enable WEBMIDI.js and trigger the onEnabled() function when ready
   WebMidi
     .enable({sysex:true})
@@ -23,7 +25,7 @@
 
       WebMidi.outputs[0].sendControlChange(0,87,1);
       WebMidi.outputs[0].sendControlChange(32, 64,1);
-      WebMidi.outputs[0].sendProgramChange(10,1);
+      WebMidi.outputs[0].sendProgramChange(118,1);
       
     }
 
@@ -36,3 +38,48 @@
     channel.playNote("C3", {duration: 1000});
       
 }
+
+function setInstrumento(){
+  let idInstrumento = getSelectedValue('listaInstrumentos');
+  console.log(idInstrumento);
+  WebMidi.outputs[0].sendControlChange(0,87,1);
+  WebMidi.outputs[0].sendControlChange(32, 64,1);
+  WebMidi.outputs[0].sendProgramChange(parseInt(idInstrumento)-1,1);
+}
+
+function carregaLista(listaId, itens){
+  const lista = document.getElementById(listaId);
+
+  itens.forEach(item => {
+  const opcao = document.createElement('option');
+  opcao.value = item.idpc;
+  opcao.text = `${item.idpc} - ${item.name}`;
+  lista.add(opcao);
+  });
+}
+
+function getSelectedText(elementId) {
+  var elt = document.getElementById(elementId);
+
+  if (elt.selectedIndex == -1)
+      return null;
+
+  return elt.options[elt.selectedIndex].text;
+}
+
+function getSelectedValue(elementId) {
+  var elt = document.getElementById(elementId);
+
+  if (elt.selectedIndex == -1)
+      return null;
+
+  return elt.options[elt.selectedIndex].value;
+}
+
+// function listaInstrumentos(){
+//   console.log('Qtd Instrumentos:', bankInstruments.length)
+// }
+
+// listaInstrumentos()
+
+carregaLista('listaInstrumentos', bankInstruments);
